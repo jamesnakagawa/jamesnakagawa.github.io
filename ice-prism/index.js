f830ae2d3c8f8b9961bd72abc03076def9761dd5
James Nakagawa <me@james.computer>
2024-05-29 18:41:51 -0700

    Completed volumetric raymarching and fixed refraction depth calculation

--- ice-prism/index.js
+++ ice-prism/index.js
@@ -3,22 +3,38 @@ import lilGui from "lilGui";
 const { THREE, Scene3D, PhysicsLoader, Project } = ENABLE3D;
 
 const glassVertex = `
+  uniform float u_incidence;
+  // uniform float u_chromatic_aberration;
+  uniform float u_refract_power;
+  uniform float u_dir;
+  uniform float u_near;
+  uniform float u_far;
+  
   varying vec3 worldNormal;
   varying vec3 eyeVector;
+  varying vec3 refraction;
+  varying vec4 worldPos;
+
   void main() {
-      vec4 worldPos = modelMatrix * vec4(position, 1.0);
-      vec4 mvPosition = viewMatrix * worldPos;
-      gl_Position = projectionMatrix * mvPosition;
-      eyeVector = normalize(worldPos.xyz - cameraPosition);
-      vec3 transformedNormal = normalMatrix * normal;
-      worldNormal = normalize(transformedNormal);
+    worldPos = modelMatrix * vec4(position, 1.0);
+    vec4 mvPosition = viewMatrix * worldPos;
+    gl_Position = projectionMatrix * mvPosition;
+    eyeVector = normalize(worldPos.xyz - cameraPosition);
+    vec3 transformedNormal = normalMatrix * normal;
+    worldNormal = normalize(transformedNormal);
+
+    refraction = refract(
+      eyeVector,
+      worldNormal, 
+      u_dir / u_incidence
+    ) * u_refract_power;
   }`;
 const glassFragment = `
   uniform float u_incidence;
   uniform vec3 u_light;
   uniform float u_saturation;
   uniform float u_chromatic_aberration;
-  uniform float u_refract_power;
+  // uniform float u_refract_power;
   uniform float u_shininess;
   uniform float u_diffuseness;
   uniform float u_fresnel_exp;
@@ -29,9 +45,12 @@ const glassFragment = `
   uniform vec3 u_color;
   uniform float u_time;
   uniform sampler2D u_texture;
+  uniform sampler2D u_back_world_positions;
 
   varying vec3 worldNormal;
   varying vec3 eyeVector;
+  varying vec3 refraction;
+  varying vec4 worldPos;
 
   vec3 diagonal(mat3 matrix) {
     return vec3(matrix[0][0], matrix[1][1], matrix[2][2]);
@@ -67,10 +86,19 @@ const glassFragment = `
     vec3 color = vec3(0.0);
     vec2 uv = gl_FragCoord.xy / u_resolution;
 
-    vec3 refractRGB = 
-      refract(eyeVector, worldNormal, u_dir / u_incidence) * u_refract_power * u_chromatic_aberration;
+    vec3 backPos = texture(u_back_world_positions, uv).xyz;
+    vec3 frontPos = worldPos.xyz;
+    float depth = length(frontPos - backPos) * u_chromatic_aberration;
 
-    vec3 rgb = texture(u_texture, uv + refractRGB.xy).rgb;
+    // gl_FragColor = vec4(depth, depth, depth, 1.0);
+
+    // vec3 refractRGB = 
+    //   refract(eyeVector, worldNormal, u_dir / u_incidence) * u_refract_power * u_chromatic_aberration;
+
+    // refractRGB = (viewMatrix * vec4(refractRGB, 1.0)).rgb;
+
+    /////////
+    vec3 rgb = texture(u_texture, uv + refraction.xy * depth).rgb;
 
     color += rgb;
     // color = sat(color, u_saturation);
@@ -117,10 +145,11 @@ const cloudsFragment = `
   varying vec4 worldPos;
   varying vec3 eyeVector;
 
-  #define SAMPLES 5.0
+  #define SAMPLES 20.0
 
   void main() {
-    vec3 backPos = texture(u_back_world_positions, gl_FragCoord.xy).xyz;
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+    vec3 backPos = texture(u_back_world_positions, uv).xyz;
     vec3 frontPos = worldPos.xyz;
     vec3 step = eyeVector / SAMPLES;
     vec3 totalMarch = backPos - frontPos;
@@ -131,7 +160,7 @@ const cloudsFragment = `
       vec4 pos = u_prism_matrix * vec4(marcher + frontPos, 1.0) + 0.5;
       color += texture(u_texture_3d, pos.xyz);
     }
-    color /= totalLength * SAMPLES;
+    color /= SAMPLES * 6.0;
 
     gl_FragColor = color;
   }
@@ -164,6 +193,80 @@ let texture3D;
   texture3D.needsUpdate = true;
 }
 
+let hslTexture3D;
+{
+  const hue2rgb = function (p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+  };
+  const hsl2rgb = function (h, s, l) {
+    let r, g, b;
+
+    if (s == 0) {
+      // achromatic (grey)
+      r = g = b = l;
+    } else {
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [r, g, b].map((x) => Math.round(x * 255));
+  };
+  // create a buffer with some data
+  const sizeX = 128;
+  const sizeY = 128;
+  const sizeZ = 128;
+
+  const data = new Uint8Array(sizeX * sizeY * sizeZ * 4);
+  let i = 0;
+
+  for (let z = 0; z < sizeZ; z++) {
+    for (let y = 0; y < sizeY; y++) {
+      for (let x = 0; x < sizeX; x++) {
+        let [r, g, b] = hsl2rgb(x / sizeX, y / sizeY, z / sizeZ);
+        data[i++] = r;
+        data[i++] = g;
+        data[i++] = b;
+        data[i++] = 255;
+      }
+    }
+  }
+
+  // use the buffer to create the texture
+  hslTexture3D = new THREE.Data3DTexture(data, sizeX, sizeY, sizeZ);
+  hslTexture3D.format = THREE.RGBAFormat;
+  hslTexture3D.needsUpdate = true;
+}
+
+let defaultBackgroundPositionTexture;
+{
+  const data = new Float32Array(4);
+
+  data[0] = -8;
+  data[1] = -8;
+  data[2] = -25;
+
+  defaultBackgroundPositionTexture = new THREE.DataTexture(data, 1, 1);
+  //   ,
+  //   THREE.ClampToEdgeWrapping,
+  //   THREE.ClampToEdgeWrapping,
+  //   THREE.NearestFilter,
+  //   THREE.NearestFilter,
+  //   THREE.RGBAFormat,
+  //   THREE.FloatType
+  // );
+  defaultBackgroundPositionTexture.type = THREE.FloatType;
+  defaultBackgroundPositionTexture.needsUpdate = true;
+}
+
 const pointer = new THREE.Vector2();
 const cloudsUniforms = {
   u_resolution: { value: { x: null, y: null } },
@@ -179,9 +282,12 @@ const glassUniforms = {
   u_time: { value: 0.0 },
   u_mouse: { value: pointer },
   u_texture: { value: null },
+  u_back_world_positions: { value: null },
+  u_near: { value: null },
+  u_far: { value: null },
   u_incidence: { value: 1.18 },
-  u_chromatic_aberration: { value: 0.2 },
-  u_refract_power: { value: 0.4 },
+  u_chromatic_aberration: { value: 0.05 },
+  u_refract_power: { value: 0.08 },
   u_saturation: { value: 1.07 },
   u_light: { value: new THREE.Vector3(1, -1, -1).normalize() },
   u_diffuseness: { value: 0.2 },
@@ -203,6 +309,7 @@ class MainScene extends Scene3D {
     this.bgTexture = new THREE.TextureLoader().load("assets/dots.png");
     this.bgTexture.mapping = THREE.EquirectangularReflectionMapping;
     this.scene.background = this.bgTexture;
+    this.bufferTarget3.texture.type = THREE.FloatType;
 
     // const prismGeometry = new THREE.BoxGeometry(3, 3, 10);
     const prismGeometry = createRoundedBox(3, 3, 10, 0.5, 36);
@@ -233,6 +340,8 @@ class MainScene extends Scene3D {
       new THREE.MeshBasicMaterial()
     );
     this.physicsPrism.clock = new THREE.Clock();
+
+    // this.unitCube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), this.clouds);
 
     this.reset();
 
@@ -273,6 +382,7 @@ class MainScene extends Scene3D {
       opacity: 0.05,
       transparent: true,
     });
+    ground.visible = false;
 
     this.physics.add.existing(this.physicsPrism);
 
@@ -281,10 +391,15 @@ class MainScene extends Scene3D {
     this.physicsPrism.body.setVelocityY(-5);
 
     this.scene.add(this.prism);
+    // this.scene.add(this.unitCube);
 
     this.camera.position.set(8, 8, 25);
+    // this.camera.position.set(0, 0, 3);
+    this.camera.far = 100;
     this.camera.updateProjectionMatrix();
     this.camera.lookAt(0, 0, 0);
+    glassUniforms.u_near.value = this.camera.near;
+    glassUniforms.u_far.value = this.camera.far;
   }
 
   update() {
@@ -294,6 +409,25 @@ class MainScene extends Scene3D {
   }
 
   preRender() {
+    // this.prism.material = this.worldPositionMaterial;
+    // this.renderer.setRenderTarget(this.bufferTarget3);
+    // this.renderer.render(this.scene, this.camera);
+
+    // cloudsUniforms.u_back_world_positions.value = this.bufferTarget3.texture;
+    // cloudsUniforms.u_prism_matrix.value
+    //   .compose(
+    //     this.prism.position,
+    //     this.prism.quaternion,
+    //     new THREE.Vector3(3, 3, 10)
+    //   )
+    //   .invert();
+
+    // this.prism.geometry.clearGroups();
+    // this.prism.geometry.addGroup(0, Infinity, 0);
+    // this.prism.material = this.clouds;
+    // this.clouds.needsUpdate = true;
+    // this.renderer.setRenderTarget(null);
+
     glassUniforms.u_time.value = this.clock.getElapsedTime();
     this.prism.visible = false;
     this.renderer.setRenderTarget(this.bufferTarget1);
@@ -308,8 +442,11 @@ class MainScene extends Scene3D {
 
     glassUniforms.u_dir.value = -1;
     glassUniforms.u_texture.value = this.bufferTarget1.texture;
-    cloudsUniforms.u_back_world_positions = this.bufferTarget3.texture;
+    glassUniforms.u_back_world_positions.value =
+      defaultBackgroundPositionTexture;
+    cloudsUniforms.u_back_world_positions.value = this.bufferTarget3.texture;
 
+    this.scene.background = null;
     cloudsUniforms.u_prism_matrix.value
       .compose(
         this.prism.position,
@@ -323,16 +460,17 @@ class MainScene extends Scene3D {
     this.prism.material = [this.glass, this.clouds];
     this.glass.side = THREE.BackSide;
     this.renderer.setRenderTarget(this.bufferTarget2);
-    // this.renderer.setRenderTarget(null);
     this.renderer.render(this.scene, this.camera);
 
     glassUniforms.u_dir.value = 1;
     glassUniforms.u_texture.value = this.bufferTarget2.texture;
+    glassUniforms.u_back_world_positions.value = this.bufferTarget3.texture;
     this.glass.side = THREE.FrontSide;
     this.prism.geometry.clearGroups();
     this.prism.geometry.addGroup(0, Infinity, 0);
     this.prism.material = this.glass;
     this.glass.needsUpdate = true;
+    this.scene.background = null;
     this.renderer.setRenderTarget(null);
   }
 
@@ -342,6 +480,8 @@ class MainScene extends Scene3D {
     const dpr = window.devicePixelRatio;
     glassUniforms.u_resolution.value.x = w * dpr;
     glassUniforms.u_resolution.value.y = h * dpr;
+    cloudsUniforms.u_resolution.value.x = w * dpr;
+    cloudsUniforms.u_resolution.value.y = h * dpr;
     this.renderer.setSize(w, h);
     this.renderer.setPixelRatio(dpr);
     this.bufferTarget1.setSize(w * dpr, h * dpr);
